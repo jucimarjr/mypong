bfecd7ecb7e6aa544b00121521355ffe3d712c00
jucimarjr <jucimar.jr@gmail.com>
2019-09-19 06:51:38 -0400

    bugfix do tracer(), ajustes de colisão, som de pontuação

--- mypong.py
+++ mypong.py
@@ -1,3 +1,9 @@
+# Jucimar Jr 2019
+# pong em turtle python https://docs.python.org/3.3/library/turtle.html
+# baseado em http://christianthompson.com/node/51
+# fonte Press Start 2P https://www.fontspace.com/codeman38/press-start-2p
+# som pontuação https://freesound.org/people/Kodack/sounds/258020/
+
 import turtle
 import os
 
@@ -6,7 +12,7 @@ screen = turtle.Screen()
 screen.title("My Pong")
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
-screen.tracer()
+screen.tracer(0)
 
 #desenhar raquete 1
 paddle_1 = turtle.Turtle()
@@ -33,12 +39,14 @@ ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0,0)
-ball.dx = 4
-ball.dy = 4
+ball.dx = 2
+ball.dy = 2
 
+# pontuação
 score_1 = 0
 score_2 = 0
 
+# head-up display da pontuação
 hud = turtle.Turtle()
 hud.speed(0)
 hud.shape("square")
@@ -51,22 +59,34 @@ hud.write("0 : 0", align="center", font=("Press Start 2P",24,"normal") )
 # mover raquete 1
 def paddle_1_up():
     y = paddle_1.ycor()
-    y += 20
+    if y < 250:
+        y += 20
+    else:
+        y = 250
     paddle_1.sety(y)
 
 def paddle_1_down():
     y = paddle_1.ycor()
-    y -= 20
+    if y > -250:
+        y += -20
+    else:
+        y = -250
     paddle_1.sety(y)
 
 def paddle_2_up():
     y = paddle_2.ycor()
-    y += 20
+    if y < 250:
+        y += 20
+    else:
+        y = 250
     paddle_2.sety(y)
 
 def paddle_2_down():
     y = paddle_2.ycor()
-    y -= 20
+    if y > -250:
+        y += -20
+    else:
+        y = -250
     paddle_2.sety(y)
 
 # mapeando as teclas
@@ -90,9 +110,9 @@ while True:
         ball.dy *= -1
     
     #colisão com parede inferior
-    if ball.ycor() < -290:
+    if ball.ycor() < -280:
         os.system("afplay bounce.wav&")
-        ball.sety(-290)
+        ball.sety(-280)
         ball.dy *= -1
 
     #colisão com parede esquerda
@@ -100,6 +120,7 @@ while True:
         score_2 += 1
         hud.clear()
         hud.write("{} : {}".format(score_1, score_2), align="center", font=("Press Start 2P",24,"normal") )
+        os.system("afplay 258020__kodack__arcade-bleep-sound.wav&")
         ball.goto(0,0)
         ball.dx *= -1
     
@@ -108,15 +129,17 @@ while True:
         score_1 += 1
         hud.clear()
         hud.write("{} : {}".format(score_1, score_2), align="center", font=("Press Start 2P",24,"normal") )
+        os.system("afplay 258020__kodack__arcade-bleep-sound.wav&")
         ball.goto(0,0)
         ball.dx *= -1
 
 
-    # colisão raquetes
-    if ball.xcor() < -340 and ball.ycor() < paddle_1.ycor() + 50 and ball.ycor() > paddle_1.ycor() - 50:
+    # colisão com raquete 1
+    if ball.xcor() < -330 and ball.ycor() < paddle_1.ycor() + 50 and ball.ycor() > paddle_1.ycor() - 50:
         ball.dx *= -1     
         os.system("afplay bounce.wav&")   
     
-    if ball.xcor() > 340 and ball.ycor() < paddle_2.ycor() + 50 and ball.ycor() > paddle_2.ycor() - 50:
+    # colisão com raquete 2
+    if ball.xcor() > 330 and ball.ycor() < paddle_2.ycor() + 50 and ball.ycor() > paddle_2.ycor() - 50:
         ball.dx *= -1
         os.system("afplay bounce.wav&")
