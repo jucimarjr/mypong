a4be49c293b09b6cb9e7c3c1a5a0c97cda52f17a
jucimarjr <jucimar.jr@gmail.com>
2019-09-18 20:32:23 -0400

    desenho da raquete e da bola

--- mypong.py
+++ mypong.py
@@ -2,12 +2,38 @@ import turtle
 import os
 
 # desenhar tela
-
 screen = turtle.Screen()
 screen.title("My Pong")
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.tracer()
+
+#desenhar raquete 1
+paddle_1 = turtle.Turtle()
+paddle_1.speed(0)
+paddle_1.shape("square")
+paddle_1.color("white")
+paddle_1.shapesize(stretch_wid=5, stretch_len=1)
+paddle_1.penup()
+paddle_1.goto(-350,0)
+
+#desenhar raquete 2
+paddle_2 = turtle.Turtle()
+paddle_2.speed(0)
+paddle_2.shape("square")
+paddle_2.color("white")
+paddle_2.shapesize(stretch_wid=5, stretch_len=1)
+paddle_2.penup()
+paddle_2.goto(350,0)
+
+#desenhar bola
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0,0)
+
 
 while True:
     screen.update()
